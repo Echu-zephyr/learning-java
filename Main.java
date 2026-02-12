0aa01e52052db8d5b409addd9f9dc81758abdb4e
Eshaan Kalpurath <ek7217@srmist.edu.in>
2026-02-12 23:54:06 +0530

    Update output format and variable comments in Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+   
         System.out.println(">> SYSTEM_OVERRIDE: NEON PROTOCOL");
-        System.out.println(">> STATUS: ONLINE");  
-        // 1. String box for text
+        System.out.println(">> STATUS: ONLINE");
+
+      
+        
+        // 1. String box for text (Name)
         String playerName = "Kael_Zero"; 
-        // 2. int box for whole numbers
+        
+        // 2. int box for whole numbers (Health)
         int health = 100;
-        // 3. double box for decimal numbers
+        
+        // 3. double box for decimals (Credits)
         int credits = 500;
 
-        // Displaying our variables
+        // Displaying the data using + (Concatenation)
         System.out.println("LOGGED IN AS: " + playerName);
         System.out.println("VITAL SIGNS: " + health + "%");
-        System.out.println("ACCOUNT BALANCE: ₩" + credits);
+        System.out.println("ACCOUNT BALANCE: $" + credits);
     }
 }
