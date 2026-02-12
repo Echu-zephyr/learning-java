ef874ee4d314be1d213f69127f329744b398fdbe
Eshaan Kalpurath <ek7217@srmist.edu.in>
2026-02-12 23:45:46 +0530

    Add status message to main method

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // This is where our Cyberpunk journey begins
         System.out.println(">> SYSTEM_OVERRIDE: NEON PROTOCOL");
-      
+        System.out.println(">> STATUS: ONLINE");
     }
 }
